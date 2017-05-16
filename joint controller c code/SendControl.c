22f60c5a7c76db42c35354ef544995a265865568
jialeihu <jialeihu2019@u.northwestern.edu>
2017-05-16 12:35:55 -0500

    joint controller c code

--- joint controller c code/SendControl.c
+++ joint controller c code/SendControl.c
@@ -2,6 +2,15 @@
 
 
 void send_control_ini(void){
+  //Timer2
+  PR2 = 249;
+  TMR2 = 0;
+  T2CONbits.TCKPS = 6;
+  IPC2bits.T2IP = 4;
+  IPC2bits.T2IS = 0;
+  IFS0bits.T2IF = 0;
+  IEC0bits.T2IE = 1;
+  
   //Timer3+OC1(PWM)
   T3CONbits.TCKPS = 0;     
   PR3 = 3999;              
@@ -12,15 +21,22 @@ void send_control_ini(void){
   OC1R = 0;              
   T3CONbits.ON = 1;   
   OC1CONbits.ON = 1;
-  
-  
+  T2CONbits.ON = 1;
   //digital output(direction bit)
   TRISDbits.TRISD6 = 0; 
-	
-	
 }
 
 
-void send_control_signal(int value){
-	 OC1RS =  value;
+void send_control_signal(int PWM_counts){
+	 
+	    if (PWM_counts >= 0)
+		{
+			OC1RS = PWM_counts*40; 
+			LATDbits.LATD6 = 1;
+		}
+		else
+		{
+			OC1RS = -PWM_counts*40; 
+			LATDbits.LATD6 = 0;
+		}
 }
--- joint controller c code/main.c
+++ joint controller c code/main.c
@@ -13,24 +13,34 @@ static float Desired_Torque = 0; // store Desired_Torque
 static float z_deg_1 = 0;        // 0 degree for abs_encoder1
 static float z_deg_2 = 0;        // 0 degree for abs_encoder2
 static float Stiffness = 0;      // store stiffness
+static float PWM_counts = 0;      // store PWM count
 
-
-
+void __ISR(_TIMER_2_VECTOR, IPL4SOFT) Controller(void){
+	
+	send_control_signal((int)PWM_counts);
+	IFS0bits.T2IF = 0;
+	
+}
 
 int main(){
+	__builtin_disable_interrupts();
 	char buffer[BUF_SIZE];
 	NU32_Startup();
 	spi_slave_init();
 	abs_encoder_spi_master_init();
+	send_control_ini();
 	unsigned int CN = 0;
     NU32_LED1 = 1;
-    NU32_LED2 = 1;
+	NU32_LED2 = 1;
+	__builtin_enable_interrupts();
 	while(1)
    {	
 	//for communication
 	//for each case send a different buffer(for following operations)
 	//if(LATEbits.LATE2 == 0){
-	CN = 0;
+	
+	__builtin_disable_interrupts();
+ 	CN = 0;
 	CN = spi_slave_io_int(0x00000026);
     switch (CN){
 	case 0x01000000:
@@ -41,8 +51,8 @@ int main(){
 	case 0x01000001:
 	{   
 	    //float temp = joint_torque
-		float temp = joint_torque(Stiffness,z_deg_1,z_deg_2);
-		spi_send_float(temp);
+		//float temp = joint_torque(Stiffness,z_deg_1,z_deg_2);
+		spi_send_float(PWM_counts);
 		break;
     }
     case 0x01000002:
@@ -78,11 +88,17 @@ int main(){
 		CHECK = 0;
 		break;
 	}  
+	case 0x01000007:
+	{	
+		PWM_counts = spi_read_float();
+		break;
+	}  
     default:
     {          
         break;
     }	
-   }
+   } 
+   __builtin_enable_interrupts();
 /* 	switch (buffer[0]) {
       case 'a': 
       {  
